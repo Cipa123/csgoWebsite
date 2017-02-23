b5d3e8908435109699c5b04b41c440867c804fb8
Cipa123 <mykolas.tamonis@gmail.com>
2017-02-23 13:42:46 -0600

    made coding style more readable

--- Sample.java
+++ Sample.java
@@ -1,45 +1,39 @@
 
-
-
-
 import java.util.Scanner;
-import java.util.*;
+//import java.util.*; // not needed
 
 public class Sample{
 	
-
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
+		int balance = 100;	
 		
+		System.out.println("Hello, and Welcome to Bet your life away.com");
 		
-		int balance = 100;
-		
-
-		
-	System.out.println("Hello, and Welcome to Bet your life away.com");
-	
-	gamble(balance);
+		gamble(balance);
 	} 
+	
 	public static void gamble(int Balance)
 	{
-	  String a = "yes";
-           Scanner sc = new Scanner(System.in);
-		  while(a.equals("yes"))
-			  
-		  {
+	        String a = "yes";
+                Scanner sc = new Scanner(System.in);
+			  while(a.equals("yes"))
+			   {
 
 				boolean Green = false;
 				boolean Red = false;
 				boolean Black = false;
+				  
 	                        System.out.println("Your balance is currently:" + Balance + ("$"));
 			  	System.out.println("How much do you want to bet?");
 	
 				int BetAmount = sc.nextInt();
 				
-				if ((BetAmount > Balance)||(BetAmount < 0))
-				{
-					System.out.println("Illigal Bet Exiting Game...");
-					a = "no";
-				}
+					if ((BetAmount > Balance)||(BetAmount < 0))
+					{
+						System.out.println("Illigal Bet Exiting Game...");
+						a = "no";
+					}
 					
 				Balance = Balance - BetAmount;
 
@@ -48,45 +42,45 @@ public class Sample{
 
 				String PlayerColor = sc.next();
 
-				 if(PlayerColor.equalsIgnoreCase("Red"))
-				 {
-					 Red = true;
-				 }
-				 else if(PlayerColor.equalsIgnoreCase("Green"))
-				 {
-					 Green = true; 
-				 }
-				 else
-				 { 
-					 Black = true;
-				 }
+					 if(PlayerColor.equalsIgnoreCase("Red"))
+					 {
+						 Red = true;
+					 }
+					 else if(PlayerColor.equalsIgnoreCase("Green"))
+					 {
+						 Green = true; 
+					 }
+					 else
+					 { 
+						 Black = true;
+					 }
 
 				 System.out.println(PlayerColor);
 
 				 int LuckyNumber = (int)(Math.random()*15);
 
-				 if ((LuckyNumber == 0)&& (Green == true))
-				 {
-					 BetAmount = BetAmount*14;
-					 Balance = Balance + BetAmount;
-					 System.out.println("JACKPOT! YOU WIN!");
-				 }
-				 else if ((LuckyNumber < 8)&& (LuckyNumber != 0) && (Red == true))
-				 {
-					 BetAmount = BetAmount*2;
-					 Balance = Balance + BetAmount;
-					 System.out.println("JACKPOT! YOU WIN!");
-				 } 
-				 else if ((LuckyNumber > 8)&& (LuckyNumber != 0)&& (Black == true))
-				 {
-					 BetAmount = BetAmount*2;
-					 Balance = Balance + BetAmount;
-					 System.out.println("JACKPOT! YOU WIN!");
-				 }
-				 else 
-				 {
-					 System.out.println("Sorry you lose"); 
-				 }
+					 if ((LuckyNumber == 0)&& (Green == true))
+					 {
+						 BetAmount = BetAmount*14;
+						 Balance = Balance + BetAmount;
+						 System.out.println("JACKPOT! YOU WIN!");
+					 }
+					 else if ((LuckyNumber < 8)&& (LuckyNumber != 0) && (Red == true))
+					 {
+						 BetAmount = BetAmount*2;
+						 Balance = Balance + BetAmount;
+						 System.out.println("JACKPOT! YOU WIN!");
+					 } 
+					 else if ((LuckyNumber > 8)&& (LuckyNumber != 0)&& (Black == true))
+					 {
+						 BetAmount = BetAmount*2;
+						 Balance = Balance + BetAmount;
+						 System.out.println("JACKPOT! YOU WIN!");
+					 }
+					 else 
+					 {
+						 System.out.println("Sorry you lose"); 
+					 }
 				 System.out.println("New Balance = " + Balance + ("$"));
 				 System.out.println("Thanks for playing!");
 				 System.out.println("This was the Lucky Number " + LuckyNumber);
