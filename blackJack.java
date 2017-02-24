920ddaf7ad0185db331893ffa6eeecd6370b578c
Cipa123 <mykolas.tamonis@gmail.com>
2017-02-24 10:39:38 -0600

    Update blackJack.java

--- blackJack.java
+++ blackJack.java
@@ -5,6 +5,11 @@ public class blackJack extends Card
 {
   private List<Card> cardList;
   
+  public blackJack()
+  {
+    cardList = new ArrayList<Card>();
+  }
+  
   public void addCard(String a, int c)
   {
    Card b = new Card(a,c);
