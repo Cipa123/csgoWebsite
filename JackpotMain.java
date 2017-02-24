6c670f49f57decea1740562bdba509e2a6a65da8
Cipa123 <mykolas.tamonis@gmail.com>
2017-02-23 18:26:23 -0600

    Update JackpotMain.java

--- JackpotMain.java
+++ JackpotMain.java
@@ -8,6 +8,7 @@ public class JackpotMain
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter number of players");
       numPlayers = sc.nextInt();
+      
       int [] bets = new int [numPlayers];
             for(int i = 0; i < bets.length; i++)
              {
