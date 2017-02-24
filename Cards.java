f3719e9ae69e690bf8c28129c20754b8286e5118
Cipa123 <mykolas.tamonis@gmail.com>
2017-02-24 10:30:05 -0600

    Update and rename Blackjack.java to Cards.java

---
+++ Cards.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Cards
+{
+  private string cardName;
+  private boolean isDrawn;
+  private int value;
+  public Cards(string a, int c)
+  {
+    cardName = a;
+    isDrawn = false;
+    value = c;
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
