a7eb3bb31d356e89e0a0cd1ba2f66649b1899a03
Cipa123 <mykolas.tamonis@gmail.com>
2017-02-27 10:36:52 -0600

    Create BlackJackTester.java

---
+++ BlackJackTester.java
@@ -0,0 +1,45 @@
+
+public class BlackJackTester
+{
+  public static void main(String[] args)
+  {
+     blackjack Game = new blackjack();
+     String a = Hearts;
+     int value = 0;
+     for(int i =0; i<53; i++)
+     {
+        Game.addCard(a, value);
+      
+      
+      }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
