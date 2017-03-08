f320afa4b7e465fb4d111602928c330ec92bcf49
a141fger <ganhamegreydalf@gmail.com>
2017-03-07 23:30:07 -0600

    Update DeckTester.java

--- DeckTester.java
+++ DeckTester.java
@@ -1,12 +1,35 @@
 public class DeckTester
 {
-    public static void main(String [] agrs)
-    {
+    public static void main(String [] agrs){
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
         Deck decck = new Deck();
-         for(int i= 0; i < 52; i++)
-         {
-             decck.draw();
-        }
+         
+        
+        
+        
+        for(int i= 0; i < 52; i++)
+        {
+        		 
+        	 decck.draw();
+        	 
+        	 
+        	 
+        	 
+        	 
+             
+         }
         
     }
 
