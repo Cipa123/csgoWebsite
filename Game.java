3c4b5484e41c7d7368f38822e081ce860f29a42b
a141fger <ganhamegreydalf@gmail.com>
2017-04-11 09:14:16 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,18 @@
-import 
+package Main;
+
+
+import javax.swing.JFrame;
+
+public class Game{
+  
+    public static void main (String[] args) {
+      
+      JFrane window = new JFrame ("Zordlon");
+      window.setContentPane (new GamePanel());
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window.setResizable(false);
+      window.pack();
+      window.setVisible(ture);
+      
+    }
+}
