b2ac4e7ce49fc30432c9ccd48ccddf6cf10f023c
Cipa123 <mykolas.tamonis@gmail.com>
2017-04-12 20:28:08 -0600

    Create PlayerObj.java

---
+++ PlayerObj.java
@@ -0,0 +1,14 @@
+
+public class PlayerObj {
+		private int health;
+		PlayerObj()
+		{
+			setHealth(10);
+		}
+		public int getHealth() {
+			return health;
+		}
+		public void setHealth(int health) {
+			this.health = health;
+		}
+}
