c083deb6bcdcb784563c17d053545aae554b34a2
Cipa123 <mykolas.tamonis@gmail.com>
2017-05-09 18:23:41 -0600

    Update Graphic.java

--- Graphic.java
+++ Graphic.java
@@ -2,36 +2,31 @@ package conversion;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+//import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-
-//import csgo.JavaSwing1.ListenForButton;
-
 import javax.swing.JLabel;
-//import javax.swing.JOptionPane;
+
 
 public class Graphic {
 	
-	public JFrame frame;
-	public JPanel panel, labels;
-	public JLabel label;
-	public JTextField jt;// = new JTextField(30);
-	public String input;
-	public JButton jb1, jb2,jb3;
-	public int choice = 0;
-	
+	private JFrame frame;
+	private JPanel panel, labels;
+	private JLabel label;
+	private JTextField jt;
+	private String input;
+	private JButton jb1, jb2,jb3;
+	private int choice = 0;
+	private ImageIcon image1;
+	private ImageIcon image2;
+	private ImageIcon image3;
 	public Graphic()
 	{
 		input = "";
@@ -47,18 +42,18 @@ public class Graphic {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		
-	//	ImageIcon oneIcon = new ImageIcon("C:\\Users\\mykolas\\workspace\\conversion\\src\\conversion\\pics\\one.png");
-	//	jb = new JButton("1", oneIcon);
-		//jb.setBorderPainted(false);
-	//	jb.setContentAreaFilled(false);
+	
+				//ImageIcon oneIcon = new ImageIcon("\\conversion\\source\\Button 1.png");
+				//	 jb = new JButton("1", oneIcon);
+				//jb.setBorderPainted(false);
+				//	jb.setContentAreaFilled(false);
 		
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		labels = new JPanel();
 		labels.setBackground(Color.white);
 		panel.setBackground(Color.white);
-	//	panel.setLayout(new GridBagLayout());
+	
 		
 		
 		
@@ -66,9 +61,19 @@ public class Graphic {
 		label = new JLabel(menuStartScreen(), JLabel.CENTER);
 		Font font = new Font("Helvetica", Font.PLAIN, 18);
 		label.setFont(font);
-		jb1 = new JButton("1");
-		jb2 = new JButton("2");
-		jb3 = new JButton("3");
+	
+		image1 = new ImageIcon(getClass().getResource("Button 1.png"));
+		jb1 = new JButton(image1);
+		jb1.setContentAreaFilled(false);
+			//jb1.setBorderPainted(false);
+		
+		image2 = new ImageIcon(getClass().getResource("Button 2.png"));
+		jb2 = new JButton(image2);
+		jb2.setContentAreaFilled(false);
+		
+		image3 = new ImageIcon(getClass().getResource("Button 3.png"));
+		jb3 = new JButton(image3);
+		jb3.setContentAreaFilled(false);
 		
 		
 		ListenForButton lForButton = new ListenForButton();
@@ -76,82 +81,44 @@ public class Graphic {
 		labels.add(jb1);
 		labels.add(jb2);
 		labels.add(jb3);
-		
+		jb1.setIcon(image1);
 		panel.add(label ,BorderLayout.NORTH);
 		panel.add(labels, BorderLayout.CENTER);
 		
-		
-		//panel.add(jt);
-	
-	//	label = new JLabel("<html>TEST <br>  TEST</html>");
-		//panel.add(jb);
-	/*	jb.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				choice = 1;
-				panel.remove(jb);
-				SwingUtilities.updateComponentTreeUI(frame);
-			}
-		});
-		/*jt.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				String input = jt.getText();
-				label.setText(input);
-			}
-		});
-		*/
-
-	//	panel.add(jb);
-		//panel.add(label);
 		frame.add(panel);
+		frame.repaint();
 		
-	}
-	public void printStuff(String a)
-	{
-		//clearStuff();
-		//SwingUtilities.updateComponentTreeUI(frame);
-		label.setText(a);
-		SwingUtilities.updateComponentTreeUI(frame);
-	
-	}
-	public void clearStuff()
-	{
-		label.setText("");
-		SwingUtilities.updateComponentTreeUI(frame);
-	}
-	public void addInput()
-	{
-		//while(getLength() == 0)
-		{
-		jt = new JTextField(30);
-		panel.add(jt);
-		jt.addActionListener(new ActionListener()
-		{
-		
-			public void actionPerformed(ActionEvent e)
-			{
-				while(getLength() == 0)
-				{
-				String enter = jt.getText();
-				setInput(enter);
-				}
-				removeInput();
-			}
-		});
-	//	panel.add(jt);
-		SwingUtilities.updateComponentTreeUI(frame);
-		}
-	
-	}
-	public void removeInput()
-	{
-		panel.remove(jt);
-		SwingUtilities.updateComponentTreeUI(frame);
+					//panel.add(jt);
+					//	label = new JLabel("<html>TEST <br>  TEST</html>");
+					//panel.add(jb);
+					/*	jb.addActionListener(new ActionListener()
+					{
+						public void actionPerformed(ActionEvent e)
+						{
+							choice = 1;
+							panel.remove(jb);
+							SwingUtilities.updateComponentTreeUI(frame);
+						}
+					});
+					/*jt.addActionListener(new ActionListener()
+					{
+						public void actionPerformed(ActionEvent e)
+						{
+							String input = jt.getText();
+							label.setText(input);
+						}
+					});
+					*/
 
+					//	panel.add(jb);
+					//panel.add(label);
+
+		
 	}
+
+	
+
+	
 	public void setInput(String b)
 	{
 		input = b;
@@ -164,11 +131,15 @@ public class Graphic {
 	{
 		return input.length();
 	}
+	
 	public void refresh()
 	{
 		SwingUtilities.updateComponentTreeUI(frame);
 	}
-	  public String menuStartScreen()
+	
+	
+	
+	 public String menuStartScreen()
 	 	{
 		  String b = "";
 	 		b +="<html><h1>------------------------------------------------------<br>" +
@@ -201,3 +172,52 @@ public class Graphic {
 		  
 	  }
 }
+
+			/* USELESS OLD CRAP
+			 * 
+			 * public void printStuff(String a)
+			{
+				//clearStuff();
+				//SwingUtilities.updateComponentTreeUI(frame);
+				label.setText(a);
+				SwingUtilities.updateComponentTreeUI(frame);
+			
+			}
+			public void clearStuff()
+			{
+				label.setText("");
+				SwingUtilities.updateComponentTreeUI(frame);
+			}
+			public void addInput()
+			{
+				//while(getLength() == 0)
+				{
+				jt = new JTextField(30);
+				panel.add(jt);
+				jt.addActionListener(new ActionListener()
+				{
+				
+					public void actionPerformed(ActionEvent e)
+					{
+						while(getLength() == 0)
+						{
+						String enter = jt.getText();
+						setInput(enter);
+						}
+						removeInput();
+					}
+				});
+				//	panel.add(jt);
+				SwingUtilities.updateComponentTreeUI(frame);
+				}
+				public void removeInput()
+				{
+					panel.remove(jt);
+					SwingUtilities.updateComponentTreeUI(frame);
+				
+				}
+				
+			*
+			*
+			*/
+
